80ff3a6d399ab59825450a5fd743f6aecbbd233b
DizzlyD <david4henery00@hotmail.com>
2022-04-18 18:52:28 +0100

    -basic-nav-functionality

--- J.S/main.js
+++ J.S/main.js
@@ -1,1 +1,24 @@
-console.log("working")
+console.log("working");
+
+
+// BASIC FUNCTIONALITY (SPLIT TO DIFFERENT MODULES)
+
+const burger = document.querySelector(".burger-menu");
+const slideNav = document.querySelector(".nav-slide");
+const exitSlideIcon = document.querySelector(".exit-icon");
+
+
+const slideNavFunctionality = function(){
+  burger.addEventListener("click", function (e) {
+    slideNav.classList.add("nav-slide-active");
+  });
+  exitSlideIcon.addEventListener("click", function(){
+    slideNav.classList.remove("nav-slide-active")
+  });
+};
+slideNavFunctionality()
+
+
+
+//***********************************************//
+
