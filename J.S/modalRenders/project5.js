a6a480ddd8ac1987ca173a79f9d22077e8fcba28
DizzlyD <david4henery00@hotmail.com>
2022-08-07 16:14:47 +0100

    ip address update

--- J.S/modalRenders/project5.js
+++ J.S/modalRenders/project5.js
@@ -1,6 +1,6 @@
 import MainProject from "./projectMain.js";
-import sonicboomImg from "../../images/project-5-imgs/sonicboom-festival.png";
-import testImg from "../../images/project-5-imgs/sonicboom-festival.netlify-test.png";
+import ipTrackerDesktop from "../../images/project-5-imgs/ip-tracker-desktop.png";
+import ipTrackerMobile from "../../images/project-5-imgs/ip-tracker-mobile.png";
 
 // SONIC BOOM PROJECT
 
@@ -14,26 +14,26 @@ class Project5 extends MainProject {
       </div>
       <div class="modal__content--img" >
         <picture>
-        <source srcset="${testImg}" media="(min-width: 700px)">
-        <source srcset="${sonicboomImg}">
-        <img src="${sonicboomImg}" alt="Flowers">
+        <source srcset="${ipTrackerMobile}" media="(min-width: 700px)">
+        <source srcset="${ipTrackerDesktop}">
+        <img src="${ipTrackerDesktop}" alt="Flowers">
         </picture>
       </div>
       <div class="modal__content--details">
-        <h4 class="modal__content--title">SonicBoom</h4>
-        <p class="modal__content--descript">SonicBoom is a fictional music festival web site. It's Primarily focused on electronic music and based in croatia.
-        It has a lot of general concepts used in most festival sites like a vibrant colour theme and photo gallerys to try and showcase the festival experience.</p>
+        <h4 class="modal__content--title">Ip Address Tracker</h4>
+        <p class="modal__content--descript">Ip address tracker is a frontend coding challenge from frontend mentor. Users can search for details about a specific Ip address, while also showing details about the users Ip address.</p>
         <ul class="modal__content--list margin1Zero">
           <h6>Technologies used:</h6>
           <li>HTML</li>
           <li>CSS/Sass</li>
           <li>Javascript</li>
-          <li>Parcel</li>
+          <li>React</li>
+          <li>Redux-Toolkit</li>
           <li>Git</li>
         </ul>
         <div class="modal__content--links">
-          <a href="https://github.com/David-Henery4/Sonicboom-Festival-Site" class="modal-btn" target="_blank" rel="noopener noreferrer">gitHub</a>
-          <a href="https://sonicboom-festival.netlify.app/" class="modal-btn" target="_blank" rel="noopener noreferrer" >Live Project</a>
+          <a href="https://github.com/David-Henery4/IP-Address-Tracker" class="modal-btn" target="_blank" rel="noopener noreferrer">gitHub</a>
+          <a href="https://main--ip-address-tracker-mentor.netlify.app" class="modal-btn" target="_blank" rel="noopener noreferrer" >Live Project</a>
         </div>
       </div>`;
   }
